671d2b9319d71e4acb2d9e9789e1be45e4160d07
postni <1524300@stud.hs-mannheim.de>
2016-11-23 21:27:23 +0100

    yeah ich glaube so gehts :D

---
+++ main.js
@@ -0,0 +1,11 @@
+
+<script language="JavaScript">
+<!--
+button1= new Image();
+button1.src = "chapterOverview-inactive.png"
+button2= new Image();
+button2.src = "chapterOverview-active.png"
+/* usw. für alle Grafiken,
+die am Mouse-Over-Effekt beteiligt sind */
+//-->
+</script>
