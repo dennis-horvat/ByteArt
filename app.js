46994d0a4abb83ca6598fdbffafd6c436cc4573d
postni <1524300@stud.hs-mannheim.de>
2016-12-04 20:58:56 +0100

    speechbubbles mitte läuft

---
+++ app.js
@@ -0,0 +1,34 @@
+var app = angular.module('app', [])
+
+app.controller('MainController', ['$scope', function($scope) {
+
+  $scope.datenJS = [
+  {
+    text: "01 - Test1 tralalalalala",
+    image: "img/chapter01/competenceUndone.png"
+  },
+  {
+    text: "02 - Test2 tralalalalala",
+    image: "img/chapter01/competenceUndone.png"
+  },
+  {
+    text: "03 - Test tralalalalala",
+    image: "img/chapter01/competenceUndone.png"
+  },
+  {
+    text: "04 - Test tralalalalala",
+    image: "img/chapter01/competenceUndone.png"
+  },
+  {
+    text: "05 - Test tralalalalala",
+    image: "img/chapter01/competenceUndone.png"
+  },
+  {
+    text: "06 - Test tralalalalala",
+    image: "img/chapter01/competenceUndone.png"
+  }
+
+
+
+];
+}]);
